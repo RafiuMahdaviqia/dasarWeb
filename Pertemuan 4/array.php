2332c6f540fd5b53315badec8e1b7319073e14bf
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2024-03-04 12:07:38 +0700

    Pertemuan 4 - Praktikum 5

---
+++ Pertemuan 4/array.php
@@ -0,0 +1,76 @@
+<?php
+// Percobaan 1
+$nilaiSiswa = [85, 92, 72, 64, 90, 55, 88, 79, 70, 96];
+$nilaiLulus = [];
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai >= 70) {
+        $nilaiLulus[] = $nilai;
+    }
+}
+
+echo "Daftar Nilai Siswa yang lulus " . implode(',', $nilaiLulus);
+echo "<br>";
+
+// Percobaan 2
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charlie', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(',', $karyawanPengalamanLimaTahun);
+echo "<br>";
+
+// Percobaan 3
+$daftarNilai = [
+    'Matematika' => [
+        ['Alice', 85],
+        ['Bob', 92],
+        ['Charlie', 78],
+    ],
+    'Fisika' => [
+        ['Alice', 90],
+        ['Bob', 88],
+        ['Charlie', 75],
+    ],
+    'Kimia' => [
+        ['Alice', 92],
+        ['Bob', 80],
+        ['Charlie', 85],
+    ],
+];
+
+$mataKuliah = 'Fisika';
+echo "Daftar nilai mahasiswa dalam mata kuliah $mataKuliah: <br>";
+foreach ($daftarNilai[$mataKuliah] as $nilai) {
+    echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
+}
+echo "<br>";
+
+// Percobaan 4
+$nilaiSiswa = [
+    'Alice' => 85,
+    'Bob' => 92,
+    'Charlie' => 78,
+    'David' => 64,
+    'Eva' => 90
+];
+
+$rataRataKelas = array_sum($nilaiSiswa) / count($nilaiSiswa);
+
+
+echo "Daftar nilai siswa yang di atas rata-rata kelas ($rataRataKelas): <br>";
+foreach ($nilaiSiswa as $nama => $nilai) {
+    if ($nilai > $rataRataKelas) {
+        echo "Nama: $nama, Nilai: $nilai <br>";
+    }
+}
