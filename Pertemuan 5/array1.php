a04a3627b41268ddf3dd6941855d9edb2288faa7
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2024-03-18 09:41:13 +0700

    Pertemuan 5 - Praktikum 1

---
+++ Pertemuan 5/array1.php
@@ -0,0 +1,19 @@
+<!Doctype HTML>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h2>Array Terindex</h2>
+	<?php
+	$Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagus Nugraha"];
+
+	foreach ($Listdosen as $dosen) {
+		echo $dosen . "<br>";
+	}
+	?>
+</body>
+</html>
