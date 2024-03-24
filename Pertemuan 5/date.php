5d38626a9fcda633e608d5727f8673e1dbe26b18
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2024-03-24 23:53:15 +0700

    Pertemuan 5 - Praktikum 9 (Date & Time)

---
+++ Pertemuan 5/date.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h3>Date</h3>
+	<?php
+		echo "Today is " . date("Y/m/d") . "<br>";
+		echo "Today is " . date("Y.m.d") . "<br>";
+		echo "Today is " . date("Y-m-d") . "<br>";
+		echo "Today is " . date("l");
+	?>
+</body>
+</html>
