af90eab946a617df1a479b1b8aa0f84e7fb2e3f8
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2024-03-24 23:56:39 +0700

    Pertemuan 5 - Praktikum 10 (Variabel)

---
+++ Pertemuan 5/variable.php
@@ -0,0 +1,31 @@
+<?php	
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP HOST'];
+echo "<br>";
+echo $_SERVER['HTTP REFERER'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
+
+<?php
+/*
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+if (isset($_SERVER['HTTP_HOST'])) {
+    echo $_SERVER['HTTP_HOST'];
+}
+echo "<br>";
+if (isset($_SERVER['HTTP_REFERER'])) {
+    echo $_SERVER['HTTP_REFERER'];
+}
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
+*/
+
+?>
