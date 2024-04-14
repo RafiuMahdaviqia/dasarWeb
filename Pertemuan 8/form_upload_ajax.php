d5c49d1389fddfd6bbd6e27f891bf8af086be15b
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2024-04-14 23:54:29 +0700

    Pertemuan 8 - Praktikum 4

--- Pertemuan 8/form_upload_ajax.php
+++ Pertemuan 8/form_upload_ajax.php
@@ -2,15 +2,18 @@
 <html>
 
 <head>
-    <title>Unggah Gambar</title>
+    <link rel="stylesheet" type="text/css" href="upload.css">
+    <title>Unggah File Dokumen</title>
 </head>
 
 <body>
-    <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="files[]" id="file" multiple="multiple" accept=".jpg,.jpeg,.png,.gif" />
-        <input type="submit" name="submit" value="Unggah">
-    </form>
-    <div id="status"></div>
+    <div class="container">
+        <h1>Unggah File Dokumen</h1>
+        <form id="upload-form" action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="file" id="file">
+            <input type="submit" name="submit" value="Unggah">
+        </form>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="upload.js"></script>
